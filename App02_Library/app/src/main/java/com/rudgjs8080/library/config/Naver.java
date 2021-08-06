742a3b3746cda1ed0e920d12b9132be0eb21f210
rudgjs8080 <kkh_8080@naver.com>
2021-08-06 11:06:34 +0900

    naver api test

--- App02_Library/app/src/main/java/com/rudgjs8080/library/config/Naver.java
+++ App02_Library/app/src/main/java/com/rudgjs8080/library/config/Naver.java
@@ -2,7 +2,7 @@ package com.rudgjs8080.library.config;
 
 public class Naver {
 
-    public static String CLIENT_ID = "adsf";
+    public static String CLIENT_ID = "sLkU5Ghgjb2TAkfAT8H6";
 
-    public static String CLIENT_SECRET = "asdf";
+    public static String CLIENT_SECRET = "nbeMolhS6d";
 }
