42376a39de5abd5775ac7a12ca4d0fa170bac771
rudgjs8080 <kkh_8080@naver.com>
2021-08-06 10:49:44 +0900

    0806 mylib naverAPI test

---
+++ App02_Library/app/src/main/java/com/rudgjs8080/library/FirstFragment.java
@@ -0,0 +1,43 @@
+package com.rudgjs8080.library;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.rudgjs8080.library.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    // fragment_first.xml 을 Binding하라 라는 의미
+    // *.xml 파일이 마치 java class 가 된 것 처럼 코딩을 할 수 있다
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
--- CaCao/app/src/main/java/com/callor/cacao/MainActivity.java
+++ CaCao/app/src/main/java/com/callor/cacao/MainActivity.java
@@ -1,12 +1,22 @@
 package com.callor.cacao;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.callor.cacao.adapter.ChattAdapter;
 import com.callor.cacao.model.Chatt;
@@ -33,6 +43,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String nickname = preferences.getString("nick_name","익명");
+        String alarm = preferences.getString("alarm","On");
+
+        Log.d("닉네임",nickname);
+        Log.d("알람",alarm);
+
+
+        /**
+         * custom 된 toolbar를 ActionBar로 설정하기 위한 코드
+         */
+        Toolbar main_toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(main_toolbar);
+
+        /**
+         * 새로운 Activity가 열렸을 때
+         * 이전 Activity(page)로 돌아가기 아이콘을 표시하기
+         */
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         txt_msg = findViewById(R.id.text_msg);
         btn_send = findViewById(R.id.btn_send);
@@ -69,7 +101,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    /**
+     * Custom 한 ToolBar가 (main)activity에 적용될 때
+     * setSupportActionBar() method가 실행될 때
+     * event가 발생하고 자동으로 호출되는 method
+     *
+     * Toolbar를 사용하여 ActionBar를 Custom
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
 
+        getMenuInflater().inflate(R.menu.main_tool_menu,menu);
+        return true;
+    }
+
+    /**
+     * ActionBar에 설정된 Option Menu의 특정한 항목(item)을
+     * 클릭하면 호출되는 method
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int menu_item = item.getItemId();
+        if(menu_item == R.id.app_bar_search){
+            Toast.makeText(this,"설정메뉴 클릭됨",Toast.LENGTH_SHORT).show();
+            Intent setting_intent = new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(setting_intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
