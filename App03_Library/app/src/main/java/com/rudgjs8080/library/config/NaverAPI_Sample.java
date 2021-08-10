2c7ce6987a30ca35a66bf0704af1e5407e192293
rudgjs8080 <kkh_8080@naver.com>
2021-08-10 09:47:34 +0900

    0810 home naverapi id,secret upload test2

---
+++ App03_Library/app/src/main/java/com/rudgjs8080/library/config/NaverAPI_Sample.java
@@ -0,0 +1,15 @@
+package com.rudgjs8080.library.config;
+
+/**
+ * Naver OpenAPI에 접근하기 위한 정보들 저장
+ * NaverOpenAPI 관련 정보 등록 변경 한 후 사용해야합니다
+ * ID, SECRET을 수정해주세요
+ */
+public class NaverAPI_Sample {
+
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
+    public static final String NAVER_BOOK_URL = "https://openapi.naver.com/v1/search/book.json";
+    public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/";
+
+}
