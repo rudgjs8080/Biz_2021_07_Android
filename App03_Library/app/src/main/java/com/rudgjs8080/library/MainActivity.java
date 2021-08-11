24f1495d68149ecd5c0cfce305cbf823476e140e
rudgjs8080 <kkh_8080@naver.com>
2021-08-11 11:40:08 +0900

    0811 app04_news naverapi test

--- App03_Library/app/src/main/java/com/rudgjs8080/library/MainActivity.java
+++ App03_Library/app/src/main/java/com/rudgjs8080/library/MainActivity.java
@@ -1,9 +1,14 @@
 package com.rudgjs8080.library;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.SearchView;
 
 import com.rudgjs8080.library.service.NaverBookService;
 import com.rudgjs8080.library.service.impl.NaverBookServiceImplV1;
@@ -12,10 +17,27 @@ public class MainActivity extends AppCompatActivity {
 
    private RecyclerView recyclerView;
 
+    /**
+     * life Cycle
+     * 폰에서 아이콘을 터치하여 App을 실행
+     * App을 사용하고, App을 종료하고
+     *
+     * App을 실행했을 때
+     * Android 가 폰에서 어플을 읽어서 메모리에 load하고
+     * 화면 구성요소를 읽어서 화면을 그리고
+     *      onCreate**() method 내에 관련된 코드 실행
+     *
+     * 각 기능을 사용하기 위한 여러가지 event 핸들러를 등록
+     *
+     */
     @Override
+    // life cycle에서 start Up Method, start point method
+    // App 이 실행될 때 가장 먼저 호출되어 코드가 실행되는 method
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar main_toolbar = findViewById(R.id.main_app_toolbar);
+        setSupportActionBar(main_toolbar);
 
         recyclerView = findViewById(R.id.book_list_view);
 
@@ -24,5 +46,77 @@ public class MainActivity extends AppCompatActivity {
             naverBookService.getBooks("코딩");
 
 
+    }
+
+    /**
+     * res/menu/menu.xml 파일을 읽어서 화면의
+     * ActionBar에 메뉴 등을 표현할 때 사용하는 method
+     *
+     * 이 method는 Android가 App을 화면에 띄울 때 자동으로
+     * 호출하여 사용하는 method
+     *
+     * activity.xml 파일에 toolbar 관련된 항목이 있으면
+     * Android는 MainActivity에 onCreateOptionsMenu() 가 있는지 확인하고
+     * 있으면 해당 method 호출하여 실행한다
+     *
+     * ActionBar에 보여줄 여러가지 항목이나, 기능등을 설정(선언)을
+     * 여기에서 수행하면 된다
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // 매개변수로 받은 menu는
+        // toolbar에 기본으로 포함된 아무것도 없는 상태의 menu 객체
+
+        // 현재 toolbar에 기본으로 포함된 blank menu 객체에
+        // main_toolbar_menu.xml 파일에 작성되어 있는 item요소를
+        // 추가하여 toolbar에 menu가 나타나도록 하여라
+        getMenuInflater().inflate(R.menu.main_toolbar_menu,menu);
+
+        /**
+         * ActionBar(AppBar)에 설정된 search 기능 구현하기
+         * 1. SearchView 객체 생성하기
+         */
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.app_bar_search)
+                .getActionView();
+        /**
+         * Integer.MAX_VALUE
+         *
+         */
+        // SearchBar width를 최대값으로 설정하여 화면에 가득차도록
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setQueryHint("도서명 검색");
+
+        /**
+         * 검색창에 값이 입력되고, 키보드의 검색을 클릭 했을 때
+         * 반응할 event 설정하기
+         */
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            // 검색창에 문자열을 입력하고
+            // 키보드의 검색 버튼을 클릭했을 때 반응
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                Log.d("검색버튼 클릭 : ", query);
+
+                NaverBookService naverBookService
+                        = new NaverBookServiceImplV1(recyclerView);
+                naverBookService.getBooks(query.trim());
+
+                return false;
+            }
+
+            // 검색창에 문자열을 입력할 때 반응하는 method
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                Log.d("현재 입력중인 문자열 : ", newText);
+
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
---
+++ App04_news/app/src/main/java/com/rudgjs8080/topnews/config/NaverAPI_sample.java
@@ -0,0 +1,11 @@
+package com.rudgjs8080.topnews.config;
+
+public class NaverAPI_sample {
+
+    public static final String CLIENT_ID = "123";
+    public static final String CLIENT_SECRET = "123";
+    public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/";
+
+
+
+}
