4be3dd0bba755eefb338cc9ee6b669b4616229d0
rudgjs8080 <kkh_8080@naver.com>
2021-08-17 16:04:24 +0900

    0817 academy2

--- App06_movie/app/src/main/java/com/rudgjs8080/movie/config/NaverAPI_Sample.java
+++ App06_movie/app/src/main/java/com/rudgjs8080/movie/config/NaverAPI_Sample.java
@@ -2,8 +2,8 @@ package com.rudgjs8080.movie.config;
 
 public class NaverAPI_Sample {
 
-    public static final String CLIENT_ID = "";
-    public static final String CLIENT_SECRET = "";
+    public static final String CLIENT_ID = "id";
+    public static final String CLIENT_SECRET = "secret";
     public static final String NAVER_BASE_URL = "https://openapi.naver.com/v1/search/";
 
 
